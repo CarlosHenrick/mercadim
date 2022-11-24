a98451b5d422735dac073766fafe433ac737abf4
Carlos Henrique Araújo Alves <carlos.alves@rasystem.com.br>
2022-11-24 16:01:43 -0300

    Exibição de senha, favicon, cursor do mouse em links e mensagem de erro do cadastro com mesmo e-mail

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://tw-integracao.azurewebsites.net',
+    baseURL: 'https://tw-integracao.azurewebsites.net'
+    // baseURL: 'https://1957-209-14-227-0.sa.ngrok.io/v1/'
 });
 
 export default api;
